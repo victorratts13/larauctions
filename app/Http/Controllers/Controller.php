64e957001419fbfeefe78812aa9126e77533bd41
victorratts13 <victor.ratts13@gmail.com>
2022-04-01 13:46:45 -0300

    test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    // initial controller
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
